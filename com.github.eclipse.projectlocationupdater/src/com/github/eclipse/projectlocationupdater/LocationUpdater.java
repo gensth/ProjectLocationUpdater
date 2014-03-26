3676a6359ffbc883fae2512c8b84d5b44163dfa7
Max Gensthaler <max@gensthaler.de>
2014-03-26 22:22:32 +0100

    LocationUpdater: whitespaces

--- com.github.eclipse.projectlocationupdater/src/com/github/eclipse/projectlocationupdater/LocationUpdater.java
+++ com.github.eclipse.projectlocationupdater/src/com/github/eclipse/projectlocationupdater/LocationUpdater.java
@@ -24,17 +24,14 @@ import org.eclipse.core.runtime.Path;
  */
 @SuppressWarnings("restriction")
 public class LocationUpdater {
-
 	/** URI prefix in location file */
 	private static final String URI_PREFIX = "URI//";
 	
 	/** URI prefix and file protocol prefix */
-	private static final String FILE_URI_PREFIX = URI_PREFIX
-			+ "file:";
+	private static final String FILE_URI_PREFIX = URI_PREFIX + "file:";
 
 	/** Constant path to the workspace projects locations storage */
-	private static final IPath WORKSPACE_PROJECT_SETTINGS_RELPATH = new Path(
-			".metadata/.plugins/org.eclipse.core.resources/.projects");
+	private static final IPath WORKSPACE_PROJECT_SETTINGS_RELPATH = new Path(".metadata/.plugins/org.eclipse.core.resources/.projects");
 
 	/**
 	 * Retrieves the path to the .location file of a project in its workspace
@@ -44,10 +41,8 @@ public class LocationUpdater {
 	 * @return An IPath to its .location file
 	 */
 	public IPath getProjectLocationFilePath(final IProject aProject) {
-
 		// Get the workspace root path
-		final IPath workspaceLocation = aProject.getWorkspace().getRoot()
-				.getLocation();
+		final IPath workspaceLocation = aProject.getWorkspace().getRoot().getLocation();
 
 		// Forge the location file name
 		return workspaceLocation.append(WORKSPACE_PROJECT_SETTINGS_RELPATH)
@@ -63,14 +58,11 @@ public class LocationUpdater {
 	 * @throws IOException
 	 *             Error reading the location file
 	 */
-	public String readProjectLocation(final IPath aLocationPath)
-			throws IOException {
-
+	public String readProjectLocation(final IPath aLocationPath) throws IOException {
 		DataInputStream in = null;
 		try {
 			// Read the location file
-			in = new DataInputStream(
-					new FileInputStream(aLocationPath.toFile()));
+			in = new DataInputStream(new FileInputStream(aLocationPath.toFile()));
 
 			// Ignore the begin chunk
 			in.skipBytes(ILocalStoreConstants.BEGIN_CHUNK.length);
@@ -78,17 +70,14 @@ public class LocationUpdater {
 			// Parse the location
 			String projectLocationStr = in.readUTF();
 			if (projectLocationStr.startsWith(FILE_URI_PREFIX)) {
-				projectLocationStr = projectLocationStr
-						.substring(FILE_URI_PREFIX.length());
-				if (systemIsWindows()
-						&& projectLocationStr.matches("^/[a-zA-Z]:")) {
+				projectLocationStr = projectLocationStr.substring(FILE_URI_PREFIX.length());
+				if (systemIsWindows() && projectLocationStr.matches("^/[a-zA-Z]:")) {
 					// remove trailing "/" from absolute path on windows
 					projectLocationStr = projectLocationStr.substring(1);
 				}
 			}
 
 			return projectLocationStr;
-
 		} finally {
 			// Be nice
 			if (in != null) {
@@ -106,9 +95,7 @@ public class LocationUpdater {
 	 * @throws IOException
 	 *             Error reading the location file
 	 */
-	public String readProjectLocation(final IProject aProject)
-			throws IOException {
-
+	public String readProjectLocation(final IProject aProject) throws IOException {
 		return readProjectLocation(getProjectLocationFilePath(aProject));
 	}
 
@@ -133,17 +120,13 @@ public class LocationUpdater {
 	 * @throws IOException
 	 *             Error reading or writing the project location file
 	 */
-	public void updateLocationSubstring(final IProject aProject,
-			final String aPreviousPrefix, final String aNewPrefix)
-			throws IOException {
-
+	public void updateLocationSubstring(final IProject aProject, final String aPreviousPrefix, final String aNewPrefix) throws IOException {
 		// Read the current location
 		final IPath locationFilePath = getProjectLocationFilePath(aProject);
 		final String currentLocation = readProjectLocation(locationFilePath);
 
 		// Replace the substring
-		final String newLocation = currentLocation.replace(aPreviousPrefix,
-				aNewPrefix);
+		final String newLocation = currentLocation.replace(aPreviousPrefix, aNewPrefix);
 
 		// Make a URI from the new path string
 		final URI newLocationURI = new Path(newLocation).toFile().toURI();
@@ -164,17 +147,14 @@ public class LocationUpdater {
 	 * @throws IOException
 	 *             Error reading or writing the location file
 	 */
-	public void writeProjectLocation(final IPath aLocationFilePath,
-			final URI aLocationURI) throws FileNotFoundException, IOException {
-
+	public void writeProjectLocation(final IPath aLocationFilePath, final URI aLocationURI) throws FileNotFoundException, IOException {
 		// Keep existing data
 		String[] referenceNames;
 
 		// Read the existing file
 		DataInputStream in = null;
 		try {
-			in = new DataInputStream(new FileInputStream(
-					aLocationFilePath.toFile()));
+			in = new DataInputStream(new FileInputStream(aLocationFilePath.toFile()));
 
 			// Ignore the begin chunk
 			in.skipBytes(ILocalStoreConstants.BEGIN_CHUNK.length);
@@ -192,7 +172,6 @@ public class LocationUpdater {
 
 			// Ignore the end chunk
 			in.skipBytes(ILocalStoreConstants.END_CHUNK.length);
-
 		} finally {
 			// Always close the file
 			if (in != null) {
@@ -203,8 +182,7 @@ public class LocationUpdater {
 		// Write the new content
 		DataOutputStream out = null;
 		try {
-			out = new DataOutputStream(new FileOutputStream(
-					aLocationFilePath.toFile()));
+			out = new DataOutputStream(new FileOutputStream(aLocationFilePath.toFile()));
 
 			// Write the begin chunk
 			out.write(ILocalStoreConstants.BEGIN_CHUNK);
@@ -221,7 +199,6 @@ public class LocationUpdater {
 			// Write the end chunk
 			out.write(ILocalStoreConstants.END_CHUNK);
 			out.flush();
-
 		} finally {
 			if (out != null) {
 				out.close();
